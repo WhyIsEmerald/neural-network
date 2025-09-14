d193d886ab95d4c42f850358e30035cc7415b2a8
WhyIsEmerald <WhyIsEmerald@proton.me>
2025-09-14 21:06:23 +0530

    file restructuring

---
+++ internals/math/func.go
@@ -0,0 +1,27 @@
+package math
+
+import "math"
+
+func Sigmoid(x float64) float64 {
+	return 1 / (1 + math.Exp(-x))
+}
+
+func Relu(x float64) float64 {
+	if x > 0 {
+		return x
+	}
+	return 0
+}
+
+func Softmax(x []float64) []float64 {
+	output := make([]float64, len(x))
+	sum := 0.0
+	for i := range x {
+		output[i] = math.Exp(x[i])
+		sum += output[i]
+	}
+	for i := range output {
+		output[i] /= sum
+	}
+	return output
+}
---
+++ internals/math/matrix.go
@@ -0,0 +1,84 @@
+package math
+
+import (
+	"errors"
+)
+
+type Matrix struct {
+	Rows int
+	Cols int
+	Data []float64
+}
+
+func NewMatrix(rows, cols int, data []float64) *Matrix {
+	return &Matrix{
+		Rows: rows,
+		Cols: cols,
+		Data: data,
+	}
+}
+
+func (m *Matrix) Get(row, col int) float64 {
+	return m.Data[row*m.Cols+col]
+}
+
+func (m *Matrix) Set(row, col int, value float64) {
+	m.Data[row*m.Cols+col] = value
+}
+
+func (m *Matrix) Add(m2 *Matrix) (*Matrix, error) {
+	if m.Rows != m2.Rows || m.Cols != m2.Cols {
+		return nil, errors.New("matrix dimensions do not match")
+	}
+	result := NewMatrix(m.Rows, m.Cols, make([]float64, m.Rows*m.Cols))
+	for i := 0; i < m.Rows; i++ {
+		for j := 0; j < m.Cols; j++ {
+			result.Set(i, j, m.Get(i, j)+m2.Get(i, j))
+		}
+	}
+	return result, nil
+}
+
+func (m1 *Matrix) Subtract(m2 *Matrix) (*Matrix, error) {
+	if m1.Rows != m2.Rows || m1.Cols != m2.Cols {
+		return nil, errors.New("Matrix dimensions do not match")
+	}
+	out := NewMatrix(m1.Rows, m1.Cols, make([]float64, m1.Rows*m1.Cols))
+	for i := 0; i < m1.Rows; i++ {
+		for j := 0; j < m1.Cols; j++ {
+			out.Set(i, j, m1.Get(i, j)-m2.Get(i, j))
+		}
+	}
+	return out, nil
+}
+
+func (m1 *Matrix) DotProduct(m2 *Matrix) (*Matrix, error) {
+	r1 := m1.Rows
+	r2 := m2.Rows
+	c1 := m1.Cols
+	c2 := m2.Cols
+	if c1 != r2 {
+		return nil, errors.New("Matrix dimensions are incompatable")
+	}
+	out := NewMatrix(r1, c2, make([]float64, r1*c2))
+	for i := 0; i < r1; i++ {
+		for j := 0; j < c2; j++ {
+			sum := 0.0
+			for k := 0; k < c1; k++ {
+				sum += m1.Get(i, k) * m2.Get(k, j)
+			}
+			out.Set(i, j, sum)
+		}
+	}
+	return out, nil
+}
+
+func (m *Matrix) Transpose() *Matrix {
+	out := NewMatrix(m.Cols, m.Rows, make([]float64, m.Rows*m.Cols))
+	for i := 0; i < m.Rows; i++ {
+		for j := 0; j < m.Cols; j++ {
+			out.Set(j, i, m.Get(i, j))
+		}
+	}
+	return out
+}
