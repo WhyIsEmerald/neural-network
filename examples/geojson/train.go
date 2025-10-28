b5997f4a348afee1a3549b97392e3606c8cc9181
WhyIsEmerald <WhyIsEmerald@proton.me>
2025-10-28 18:56:28 +0530

    Extracted reused code into functions

--- examples/geojson/train.go
+++ examples/geojson/train.go
@@ -1,103 +1,27 @@
 package geojson
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"time"
 
-	"github.com/whyisemerald/neural_network/internals/network"
 	"slices"
+
+	"github.com/whyisemerald/neural_network/internals/network"
 )
 
 func Train() {
 	rand.Seed(time.Now().UnixNano())
 
-	// Load GeoJSON data
-	file, err := ioutil.ReadFile(GeojsonPath)
+	// Load and extract GeoJSON data
+	geoData, err := LoadAndExtractGeoJSON(GeojsonPath)
 	if err != nil {
 		panic(err)
-	}
-
-	var featureCollection FeatureCollection
-	err = json.Unmarshal(file, &featureCollection)
-	if err != nil {
-		panic(err)
-	}
-
-	// Extract polygons and find bounding box
-	var multiPolygons []MultiPolygon
-	minLon, minLat := 180.0, 90.0
-	maxLon, maxLat := -180.0, -90.0
-
-	for _, feature := range featureCollection.Features {
-		var multiPolygon MultiPolygon
-		switch feature.Geometry.Type {
-		case "Polygon":
-			var polygon Polygon
-			if err := json.Unmarshal(feature.Geometry.Coordinates, &polygon); err != nil {
-				panic(err)
-			}
-			multiPolygon = MultiPolygon{polygon}
-		case "MultiPolygon":
-			if err := json.Unmarshal(feature.Geometry.Coordinates, &multiPolygon); err != nil {
-				panic(err)
-			}
-		}
-
-		for _, polygon := range multiPolygon {
-			for _, ring := range polygon {
-				for _, point := range ring {
-					lon, lat := point[0], point[1]
-					if lon < minLon {
-						minLon = lon
-					}
-					if lon > maxLon {
-						maxLon = lon
-					}
-					if lat < minLat {
-						minLat = lat
-					}
-					if lat > maxLat {
-						maxLat = lat
-					}
-				}
-			}
-		}
-		multiPolygons = append(multiPolygons, multiPolygon)
 	}
 
 	// Generate training data
-	inputs := make([][]float64, NumSamples)
-	expected := make([][]float64, NumSamples)
-	numClasses := len(featureCollection.Features)
-
-	for i := 0; i < NumSamples; i++ {
-		var point Point
-		var regionIndex int
-		for {
-			lon := minLon + rand.Float64()*(maxLon-minLon)
-			lat := minLat + rand.Float64()*(maxLat-minLat)
-			point = Point{lon, lat}
-
-			found := false
-			for j, mp := range multiPolygons {
-				if IsPointInMultiPolygon(point, mp) {
-					regionIndex = j
-					found = true
-					break
-				}
-			}
-			if found {
-				break
-			}
-		}
-
-		inputs[i] = []float64{point[0], point[1]}
-		expected[i] = make([]float64, numClasses)
-		expected[i][regionIndex] = 1
-	}
+	inputs, expected := GenerateTrainingData(geoData, NumSamples)
+	numClasses := len(geoData.FeatureCollection.Features)
 
 	// Create or load the network
 	var n *network.Network
